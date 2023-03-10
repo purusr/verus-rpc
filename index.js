e3f5ffdc78ae978b02608341f768a2a65787e91e
Rankrish <59313760+purusr@users.noreply.github.com>
2023-03-11 00:56:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+const { spawn } = require('child_process');
+const express = require('express');
+const app = express()
+const cors = require('cors')
+
+app.use(express.json())
+app.use((express.urlencoded({extended: true})))
+app.use(cors())
+
+const send_verus = (address, amount) =>{
+let ct =''
+let ft =''
+let st =''
+let sndt =''
+let transactionstatus = ''
+const ctInit = spawn(`verus createrawtransaction "[]" "{\"${address}\":${amount}}"`);
+const ftInit = spawn(`verus fundrawtransaction ${ct}`)
+const stInit = spawn(`verus signrawtransaction ${ft}`)
+const sndInit =spawn(` verus sendrawtransaction ${st}`)
+//initiate createrawtransaction
+
+ctInit.stdout.on('data', (data) => {
+  ct=data;
+});
+
+//initiate fundrawtransaction
+
+ftInit.stdout.on('data', (data) => {
+  ft=data.hex;
+});
+
+//initiate signrawtransaction
+stInit.stdout.on('data', (data) => {
+  st=data.hex;
+  if(data.complete){
+    transactionstatus = "Transaction completed successfully"
+  }else{
+    transactionstatus = "Transaction failed, something went wrong."
+  }
+});
+
+//finally send rawtransaction
+sndInit.stdout.on('data', (data) => {
+  sndt=data;
+});
+
+return transactionstatus;
+}
+
+
+app.post('/pay_verus_craft', (req, res) =>{
+    const {amount, address} = req.body;
+    console.log(amount)
+    if (amount == undefined || address == undefined){
+       res.json({data: 'No possible action'})
+    }else{
+        const processPayment = send_verus(address, amount);
+        res.json({data: processPayment})
+    }
+     
+})
+
+
+app.listen(8001,() =>{
+    console.log('server running on port 8001')
+})
